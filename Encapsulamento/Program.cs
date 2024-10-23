cd8515ad6ba1858c754fe0dbc57c0f8e5973500c
girse <girse@GIRSELY>
2024-10-23 15:38:21 -0300

    Adicionar arquivos de projeto.

---
+++ Encapsulamento/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "Girsely Máximo";
+            pessoa.Idade = 26;
+            pessoa.Telefone = "(81)985607671";
+            pessoa.Endereco = "Rua 40,N 85";
+            pessoa.Email = "girsely@gmail.com";
+
+            Console.WriteLine($"Nome: {pessoa.Nome}");
+            Console.WriteLine($"Idade: {pessoa.Idade}");
+            Console.WriteLine($"Telefone: {pessoa.Telefone}");
+            Console.WriteLine($"Endereço: {pessoa.Endereco}");
+            Console.WriteLine($"E-mail: {pessoa.Email}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+    }
+}
+// No Main, usei um bloco try-catch para capturar possíveis exceções geradas durante a configuração dos atributos.
+//Com isso, a classe Pessoa está encapsulada corretamente, e as validações são realizadas conforme solicitado.
